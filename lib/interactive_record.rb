2d3551dd63203b2cf2ce9ad3c406c02578870029
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 18:00:22 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,7 +15,7 @@ class InteractiveRecord
     column_names = []
     
     table_info.each do |column|
-    column_names << column["name"] 
+    column_names << column["name"] # how does this work??
   end 
   
   
