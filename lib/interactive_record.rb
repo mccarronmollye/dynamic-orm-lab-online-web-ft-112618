5ab346077c317d32f68e13be29e52e643de650e5
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 18:37:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,8 @@ class InteractiveRecord
     values = []
     self.class.col_names.each do |col_name|
       values << "'#{send(col_name}'" unless send(col_name).nil?
-      
+    end 
+    
   end 
   
   
