3f3dab65ecf249335c9dea822d76c827bba9c996
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 18:44:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -47,7 +47,7 @@ class InteractiveRecord
     
     DB[:conn].execute(sql)
     
-    @id =  DB[:conn].execute(SELECT last_insert_rowid() FROM #{)
+    @id =  DB[:conn].execute("SELECT last_insert_rowid() FROM #{}")
   end 
   
   
