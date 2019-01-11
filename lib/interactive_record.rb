3e03b1fd9645cca0c3c056b8e1c0bba24229f43d
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 18:39:11 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,7 @@ class InteractiveRecord
     
   def values_for_insert
     values = []
-    self.class.col_names.each do |col_name|
+    self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end 
     values.join(", ")
