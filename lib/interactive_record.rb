e85f8a40d232265aac0a7d9d1ea124fd206fa97b
Molly McCarron <mccarronmollye@gmail.com>
2019-01-11 18:41:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,7 @@ class InteractiveRecord
   end 
   
   def save
-    sql = "INSERT INTO #{table_name_for_insert} "
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})"
   end 
   
   
